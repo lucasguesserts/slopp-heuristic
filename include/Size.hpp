167b7ca8c7614c5cb4137b4b13f14c5454bc4f4d
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-05-01 12:43:44 -0300

    feat: add 'volume' to 'Size'

--- include/Size.hpp
+++ include/Size.hpp
@@ -12,11 +12,17 @@ public:
     [[nodiscard]] auto x() const -> SizeType {
         return this->_x;
     }
+
     [[nodiscard]] auto y() const -> SizeType {
         return this->_y;
     }
+
     [[nodiscard]] auto z() const -> SizeType {
         return this->_z;
+    }
+
+    [[nodiscard]] auto volume() const -> SizeType {
+        return this->x() * this->y() * this->z();
     }
 
 private:
--- test/Size.test.cpp
+++ test/Size.test.cpp
@@ -5,11 +5,12 @@
 using namespace packing;
 
 TEST_CASE("constructor", "[Size]") {
-    const auto x = SizeType{1};
-    const auto y = SizeType{1};
-    const auto z = SizeType{1};
+    const auto x = SizeType{2};
+    const auto y = SizeType{3};
+    const auto z = SizeType{5};
     const auto size = Size{x, y, z};
     CHECK(size.x() == x);
     CHECK(size.y() == y);
     CHECK(size.z() == z);
+    CHECK(size.volume() == 30);
 }
