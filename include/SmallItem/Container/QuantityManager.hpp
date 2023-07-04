2061dbdc7bb7e865477ce46067e38076f368b891
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-07-04 15:40:46 -0300

    feat: add class 'QuantityManager'

---
+++ include/SmallItem/Container/QuantityManager.hpp
@@ -0,0 +1,49 @@
+#ifndef SMALL_ITEMS_QUANTITY_MANAGER_HPP_
+#define SMALL_ITEMS_QUANTITY_MANAGER_HPP_
+
+#include <cstddef>
+#include <stdexcept>
+#include <unordered_map>
+
+#include <boost/functional/hash.hpp>
+
+#include "Type/Quantity.hpp"
+
+namespace packing {
+
+template <typename ItemTypePtr, typename ItemTypeHash>
+class SmallItemQuantityManager : private std::unordered_map<ItemTypePtr, Quantity, ItemTypeHash> {
+public:
+    auto add_item(const ItemTypePtr & small_item) -> void {
+        if (small_item->quantity() == 0) {
+            throw std::runtime_error("SmallItemQuantityManager::add_item: small item quantity is zero");
+        }
+        if (this->find(small_item) == this->end()) {
+            (*this)[small_item] = small_item->quantity();
+        } else {
+            (*this)[small_item] += small_item->quantity();
+        }
+        return;
+    }
+
+    auto remove_one_item(const ItemTypePtr & small_item) -> void {
+        if (this->find(small_item) == this->end()) {
+            throw std::runtime_error("SmallItemQuantityManager::remove_one_item: small item not found");
+        }
+        (*this)[small_item] -= 1;
+        if ((*this)[small_item] == 0) {
+            this->erase(small_item);
+        }
+        return;
+    }
+    auto quantity(const ItemTypePtr & small_item) -> Quantity {
+        if (this->find(small_item) == this->end()) {
+            return 0;
+        }
+        return (*this)[small_item];
+    }
+};
+
+} // namespace packing
+
+#endif
---
+++ test/SmallItem/Container/QuantityManager.test.cpp
@@ -0,0 +1,33 @@
+#include "Test/Test.hpp"
+
+#include <vector>
+
+#include "SmallItem/BasicSmallItem.hpp"
+#include "SmallItem/Container/QuantityManager.hpp"
+
+using namespace packing;
+
+TEST_CASE("small item quantity", "[SmallItemQuantityManager]") {
+    // This does not test the quantity manager's ability to handle
+    // repeated small items, missing small items,
+    // or small items with zero quantity.
+    const auto small_items = std::vector<BasicSmallItem::Ptr>{
+        std::make_shared<BasicSmallItem>(Vector3D{1, 2, 3}, 3),
+        std::make_shared<BasicSmallItem>(Vector3D{1, 2, 4}, 4),
+        std::make_shared<BasicSmallItem>(Vector3D{1, 3, 3}, 8),
+        std::make_shared<BasicSmallItem>(Vector3D{4, 2, 3}, 1),
+    };
+    auto quantity_manager = SmallItemQuantityManager<SmallItemType::Ptr, SmallItemType::Hash>();
+    for (auto & small_item : small_items) {
+        quantity_manager.add_item(small_item);
+    }
+    for (auto & small_item : small_items) {
+        CHECK(quantity_manager.quantity(small_item) == small_item->quantity());
+    }
+    for (auto & small_item : small_items) {
+        quantity_manager.remove_one_item(small_item);
+    }
+    for (auto & small_item : small_items) {
+        CHECK(quantity_manager.quantity(small_item) == small_item->quantity() - 1);
+    }
+}
