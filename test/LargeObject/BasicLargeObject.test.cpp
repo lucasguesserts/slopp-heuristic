be837cae1d453cee729af4a86288948546acba44
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-07-04 10:28:39 -0300

    test: add test to class 'BasicLargeObject'

---
+++ test/LargeObject/BasicLargeObject.test.cpp
@@ -0,0 +1,12 @@
+#include "Test.hpp"
+
+#include "LargeObject/BasicLargeObject.hpp"
+#include "Vector3D.hpp"
+
+using namespace packing;
+
+TEST_CASE("constructor", "[LargeObject]") {
+    const auto measurement = Vector3D{1, 2, 3};
+    const auto large_object = BasicLargeObject{measurement};
+    CHECK(large_object.measurement() == measurement);
+}
